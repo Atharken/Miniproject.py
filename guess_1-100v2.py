a95e8878d12e80bbf8fbb72779677638daa9f7d8
Athar <157311908+Atharken@users.noreply.github.com>
2026-08-11 00:25:20 +0530

    Add files via upload

---
+++ guess_1-100v2.py
@@ -0,0 +1,30 @@
+import random
+
+x = random.randint(1, 100)
+attempt = 0
+try:
+  while True:
+           
+      main = int(input("enter your number\ntype 0 to leave\n:"))
+
+
+      if main == x:
+          print(f"your guess is correct {x}")
+          print(f"you took {attempt} attempts ")
+          break
+      elif main == 0:
+          print("thanks for using our code")
+          print(f"you took {attempt} attempts ")
+          break
+      elif attempt > 7:
+          print("no attempts are left\n game over!")
+          break
+          
+      elif main > x:
+          print("too high!!!")
+          attempt += 1
+      elif main < x:
+          print("too low!!!")
+          attempt += 1
+except ValueError:
+    print("invalid input")

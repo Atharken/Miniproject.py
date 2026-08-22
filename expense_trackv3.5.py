6b43be96b2571d95f4568674e7fd8f5dea514a33
Athar <157311908+Atharken@users.noreply.github.com>
2026-08-22 05:44:04 -0700

    Add files via upload

---
+++ expense_trackv3.5.py
@@ -0,0 +1,64 @@
+#v4 fixed some errors and add a remove feature
+l = [{"name" : "kk","amount" : 1000,"category" : "music"},{"name" : "arjit","amount" : 1000,"category" : "music"},{"name" : "milk","amount" : 100,"category" : "grocery"}] 
+
+while True:
+    main = input("1.add expense\n2.view expense\n3.calculate total\n4.search by category\n5.remove expense\n6.exit\n:")
+
+    if main == "1":
+        name = input("name:")
+        try:
+          amount = int(input("amount:"))
+        except ValueError:
+            print("wrong input !!!")
+            continue   
+        if amount < 1:
+            print("invalid amount")
+            continue 
+        category = input("category:")
+        expense = {"name" : name,"amount" : amount,"category" : category}
+        l.append(expense)
+    elif main == "2":
+        for index, value in enumerate(l):
+          print(f"index {index + 1} : {value['name']} | {value['amount']} | {value['category']}")
+    elif main == "3":
+        total = 0
+        for i in (l):
+            total = total + i["amount"]
+        print(f"your total expense is ${total}")
+    elif main == "4":
+        found = False
+        main2 = input("enetr your category\n:")
+        for i in (l):
+            if i["category"] == main2:
+              print(f"{i['name']} | {i['amount']} | {i['category']}")
+              found = True
+        if found == False:
+          print(f"theres no {main2} category") 
+    elif main == "5":
+       main3 = input("enter your expense category\n:")
+       found = False
+
+       for key, value in enumerate(l):
+          if value["category"] == main3:
+             print(f"{key + 1}  {value}")
+             found = True
+
+
+          if found == False:
+             print(f"theres no such category as {main3}")
+             continue
+              
+
+          try:
+            remove = int(input("enter the sequence number of your item\n:"))
+          except ValueError:
+            print("invalid input")
+            continue
+          removeon = l.pop(remove - 1)
+
+
+                
+    elif main == "6":
+        print("thanks for using our code")
+        break
+        

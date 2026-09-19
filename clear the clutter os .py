3a9e1b3bbffed1c5ca796ecbb21793f0f847d0aa
Athar <157311908+Atharken@users.noreply.github.com>
2026-09-19 04:33:48 -0700

    next making unique dir

--- clear the clutter os .py
+++ clear the clutter os .py
@@ -8,7 +8,10 @@ print(os.getcwd())
 
 x = os.listdir(r"D:\clear the clutter")
 
+
 for i in x:
  print(i)
-
-
+ x1, y = os.path.splitext(r"D:\clear the clutter")
+ print(x1, y)
+ f = os.path.splitext(i)
+ print(f)

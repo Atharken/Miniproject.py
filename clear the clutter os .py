a2db69677a5786896dc8df9ca0da293110f545a7
Athar <157311908+Atharken@users.noreply.github.com>
2026-09-19 10:32:49 -0700

    Update clear the clutter os .py

--- clear the clutter os .py
+++ clear the clutter os .py
@@ -1,5 +1,13 @@
 import os
 
+l = {
+ "audio" : [".mp3", ".wav", ".m4a"],
+ "images" : [".png", ".jpg", ".jpeg", ".gif", ".webp"],
+ "videos" : [".mp4", ".mkv", ".avi", ".mov"],
+ "documents" : [".pdf", ".docx", ".doc", ".txt"],
+ "archives" : [".zip", ".rar", ".7z"],
+ "code" : [".py", ".js", ".html", ".css"]
+}
 print(os.getcwd())
 
 os.chdir(r"D:\clear the clutter")
@@ -10,8 +18,14 @@ x = os.listdir(r"D:\clear the clutter")
 
 
 for i in x:
- print(i)
- x1, y = os.path.splitext(r"D:\clear the clutter")
- print(x1, y)
+ 
  f = os.path.splitext(i)
- print(f)
+ #print(f[1])
+
+ for ii in l.items():
+    for iii in ii[1]:
+      if f[1] == iii:
+        print("true")
+      
+      if f[1] == iii:
+        print("true")
